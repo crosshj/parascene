8bf1c1dc2a91ade8ad7d234f999b150daf00ed97
crosshj <github@chimpjuice.com>
2026-09-23 15:42:47 -0400

    vps: inject vars when serving html

--- vps/routes/pages.js
+++ vps/routes/pages.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "node:fs/promises";
 import path from "node:path";
 
 const PAGE_ROUTES = {
@@ -8,15 +9,40 @@ const PAGE_ROUTES = {
 	"/auth.html": "auth/auth.html"
 };
 
+const DEFAULT_CANONICAL_ORIGIN = "https://www.parascene.com";
+
+function escapeHtml(value) {
+	return String(value)
+		.replaceAll("&", "&amp;")
+		.replaceAll("<", "&lt;")
+		.replaceAll(">", "&gt;")
+		.replaceAll('"', "&quot;")
+		.replaceAll("'", "&#39;");
+}
+
+function canonicalUrlForRequest(req) {
+	const pathOnly = (req.originalUrl || req.path || "/").split("?")[0] || "/";
+	const origin = (process.env.APP_ORIGIN || DEFAULT_CANONICAL_ORIGIN).replace(/\/$/, "");
+	return `${origin}${pathOnly.startsWith("/") ? pathOnly : `/${pathOnly}`}`;
+}
+
+async function sendPage(req, res, next, filePath) {
+	try {
+		let html = await fs.readFile(filePath, "utf8");
+		html = html.replace("{{CANONICAL_LINK}}", `<link rel="canonical" href="${escapeHtml(canonicalUrlForRequest(req))}" />`);
+		res.type("html").send(html);
+	} catch (error) {
+		next(error);
+	}
+}
+
 /** Serve page modules and their CSS/JS/assets from pages/<page>/. */
 export default function createPageRoutes({ pagesDir }) {
 	const router = express.Router();
 
 	for (const [route, relativePath] of Object.entries(PAGE_ROUTES)) {
 		router.get(route, (req, res, next) => {
-			res.sendFile(path.join(pagesDir, relativePath), (error) => {
-				if (error) next(error);
-			});
+			sendPage(req, res, next, path.join(pagesDir, relativePath));
 		});
 	}
 
